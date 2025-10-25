1db41200f65296171e7dec6c26d0e1d66de2b2f6
Khalid-lazrag <khalid.lazrag@etu.uae.ac.ma>
2025-10-25 14:30:39 +0100

    feat: add PostHog integration with configuration and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,32 @@
 import type { NextConfig } from "next";
 import createMDX from '@next/mdx'
 
-
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
+
+  // Add PostHog rewrites for ingestion endpoints
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://eu.i.posthog.com/:path*',
+      },
+    ];
+  },
+
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
-})
+});
 
 // Merge MDX config with Next.js config
 export default withMDX(nextConfig);
